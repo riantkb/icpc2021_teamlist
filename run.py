dec93fe8ff133dcb40ed6fc9a5c8373a59588b8d
riantkb <kubota.rian@gmail.com>
2021-11-04 23:28:30 +0900

    initial commit

---
+++ run.py
@@ -0,0 +1,179 @@
+import os
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+import time
+import datetime
+import pickle
+
+import rating_utils
+
+valid_chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_'
+
+
+pickle_path = 'atcoder.pickle'
+
+pickle_atcoder = {}
+if os.path.isfile(pickle_path):
+    with open(pickle_path, 'rb') as f:
+        pickle_atcoder = pickle.load(f)
+
+
+def convertFromRatingToSpan(rating):
+    if rating <= 0:
+        return f'<span class="user-unrated">{rating}</span>'
+    elif rating < 400:
+        return f'<span class="user-gray">{rating}</span>'
+    elif rating < 800:
+        return f'<span class="user-brown">{rating}</span>'
+    elif rating < 1200:
+        return f'<span class="user-green">{rating}</span>'
+    elif rating < 1600:
+        return f'<span class="user-cyan">{rating}</span>'
+    elif rating < 2000:
+        return f'<span class="user-blue">{rating}</span>'
+    elif rating < 2400:
+        return f'<span class="user-yellow">{rating}</span>'
+    elif rating < 2800:
+        return f'<span class="user-orange">{rating}</span>'
+    else:
+        return f'<span class="user-red">{rating}</span>'
+
+
+def fetchUserPage(ulink):
+    if (ulink in pickle_atcoder
+        and datetime.datetime.now() - pickle_atcoder[ulink]['datetime']
+            < datetime.timedelta(hours=12)):
+        # print(f'pickle loaded    link: {ulink}', file=sys.stderr)
+        response = pickle_atcoder[ulink]['response']
+    else:
+        time.sleep(1)
+        response = requests.get(ulink)
+        if response.status_code == requests.codes.ok or response.status_code == requests.codes.not_found:
+            pickle_atcoder[ulink] = {
+                'response': response,
+                'datetime': datetime.datetime.now()
+            }
+
+    return response
+
+
+def getUserRate(username):
+    if not username:
+        return 0
+    reqname = ''.join([c for c in username if c in valid_chars])
+    if reqname == '':
+        return 0
+
+    ulink = f'https://atcoder.jp/users/{reqname}'
+    response = fetchUserPage(ulink)
+    if response.status_code != requests.codes.ok:
+        return 0
+
+    return int(pd.read_html(response.text)[1].iloc[1, 1])
+
+
+def getUserSpan(username):
+    if not username:
+        return ''
+    reqname = ''.join([c for c in username if c in valid_chars])
+    if reqname == '':
+        return username
+
+    ulink = f'https://atcoder.jp/users/{reqname}'
+    response = fetchUserPage(ulink)
+    if response.status_code != requests.codes.ok:
+        return username
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    uinfo = soup.select_one('a.username')
+    icon = str(uinfo.previous_sibling.previous_sibling)
+    if '//img.atcoder.jp/assets/icon/crown_' in icon:
+        icon = icon.replace(
+            '//img.atcoder.jp/assets/icon/crown_',
+            'https://img.atcoder.jp/assets/icon/crown_'
+        )
+    else:
+        icon = ''
+    return f'{icon} <a href="{ulink}">{str(uinfo.span)}</a>'
+
+
+url = 'https://jag-icpc.org/?2021%2FTeams%2FList'
+df = pd.read_html(url)[1].fillna('')[5:].reset_index(drop=True)
+df = df.rename(columns={'メンバー 1': 'メンバー1', 'コーチ，ココーチ': 'コーチ'})
+res_df = df.copy()
+user_columns = ['メンバー1', 'メンバー2', 'メンバー3', 'コーチ']
+res_df['チームレート'] = ''
+for i in range(len(df)):
+    for c in user_columns:
+        username = df[c][i]
+        res_df.loc[res_df.index[i], c] = getUserSpan(username)
+    ratings = []
+    for c in user_columns[:-1]:
+        username = df[c][i]
+        ratings.append(getUserRate(username))
+
+    res_df.loc[res_df.index[i], 'チームレート'] = convertFromRatingToSpan(
+        int(rating_utils.aggregateRatings(ratings)))
+
+res_df = res_df.reindex(
+    columns=[
+        'チーム名',
+        '学校名',
+        'メンバー1',
+        'メンバー2',
+        'メンバー3',
+        'コーチ',
+        'チームレート',
+        'ひとこと等'
+    ]
+)
+
+df_html = res_df.to_html().replace('&lt;', '<').replace('&gt;', '>')
+
+complete_html = """
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.0/js/jquery.tablesorter.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.0/css/theme.default.min.css">
+<title>ICPC 2021 Domestic Teams</title>
+<style>
+table a {text-decoration: none;}
+.username > span {font-weight:bold;}
+a:hover.username {text-decoration: none;}
+.user-red {color:#FF0000;}
+.user-orange {color:#FF8000;}
+.user-yellow {color:#C0C000;}
+.user-blue {color:#0000FF;}
+.user-cyan {color:#00C0C0;}
+.user-green {color:#008000;}
+.user-brown {color:#804000;}
+.user-gray {color:#808080;}
+.user-unrated {color:#000000;}
+.user-admin {color:#C000C0;}
+.crown-gold {color: #fb0;}
+.crown-silver {color: #aaa;}
+.dataframe {font-size: 16px; }
+</style>
+<script>
+$(document).ready(function() {
+    $('.dataframe').tablesorter();
+});
+</script>
+</head>
+<body>
+<p>This information is from <a href="https://jag-icpc.org/?2021%%2FTeams%%2FList">https://jag-icpc.org/?2021%%2FTeams%%2FList</a> (Last Update: %s)</p>
+<p><b>情報の真偽に対する一切の責任を負いません</b></p>
+%s
+</body>
+</html>
+""" % (datetime.datetime.now(), df_html)
+
+with open('index.html', 'w') as f:
+    f.write(complete_html)
+
+with open(pickle_path, 'wb') as f:
+    pickle.dump(pickle_atcoder, f)
